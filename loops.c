25bad01159ddd50fc8a2ec93a290521a8887ed8e
Sushant Gupta <sushantg339@gmail.com>
2024-02-01 19:07:14 +0530

    new code added

---
+++ loops.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+    int x ;
+    printf("enter the number : ");
+    scanf("%d",&x);
+
+    for(int i = 1 ; i <= x ; i++){
+        printf("hello ji!\n");
+    }
+}
